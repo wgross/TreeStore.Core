9e755ed2c3fafc792f3207bcad4e06302d8d08d9
Wolfgang Gross <github@text-mode.de>
2021-09-19 11:20:33 +0200

    [TEST] integ test with item property commands

--- test/TreeStore.DictionaryFS.Test/PropertyCmdletProvider/PropertyCmdletProviderTest.cs
+++ test/TreeStore.DictionaryFS.Test/PropertyCmdletProvider/PropertyCmdletProviderTest.cs
@@ -9,6 +9,30 @@ namespace TreeStore.DictionaryFS.Test.PropertyCmdletProvider
     public class PropertyCmdletProviderTest : DynamicPropertyCmdletProviderTestBase
     {
         [Fact]
+        public void Powershell_creates_item_property()
+        {
+            // ARRANGE
+            var root = new UnderlyingDictionary
+            {
+            };
+
+            this.ArrangeFileSystem(root);
+
+            // ACT
+            var result = this.PowerShell.AddCommand("New-ItemProperty")
+                .AddParameter("Path", @"test:\")
+                .AddParameter("Name", "data")
+                .AddParameter("Value", "text")
+                .Invoke()
+                .ToArray();
+
+            // ASSERT
+            Assert.False(this.PowerShell.HadErrors);
+            Assert.True(root.TryGetValue("data", out var value));
+            Assert.Equal("text", value);
+        }
+
+        [Fact]
         public void PowerShell_clears_item_property_value()
         {
             // ARRANGE
@@ -83,6 +107,117 @@ namespace TreeStore.DictionaryFS.Test.PropertyCmdletProvider
             Assert.False(this.PowerShell.HadErrors);
             Assert.True(root.TryGetValue("data", out var value));
             Assert.Equal("text", value);
+        }
+
+        [Fact]
+        public void Powershell_removes_item_property()
+        {
+            // ARRANGE
+            var root = new UnderlyingDictionary
+            {
+                ["data"] = 1
+            };
+
+            this.ArrangeFileSystem(root);
+
+            // ACT
+            var result = this.PowerShell.AddCommand("Remove-ItemProperty")
+                .AddParameter("Path", @"test:\")
+                .AddParameter("Name", "data")
+                .Invoke()
+                .ToArray();
+
+            // ASSERT
+            Assert.False(this.PowerShell.HadErrors);
+            Assert.False(root.TryGetValue("data", out var _));
+        }
+
+        [Fact]
+        public void Powershell_renames_item_property()
+        {
+            // ARRANGE
+            var root = new UnderlyingDictionary
+            {
+                ["data"] = 1
+            };
+
+            this.ArrangeFileSystem(root);
+
+            // ACT
+            var result = this.PowerShell.AddCommand("Rename-ItemProperty")
+                .AddParameter("Path", @"test:\")
+                .AddParameter("Name", "data")
+                .AddParameter("NewName", "data-changed")
+                .Invoke()
+                .ToArray();
+
+            // ASSERT
+            Assert.False(this.PowerShell.HadErrors);
+            Assert.True(root.TryGetValue("data-changed", out var value));
+            Assert.Equal(1, value);
+        }
+
+        [Fact]
+        public void Powershell_copies_item_property()
+        {
+            // ARRANGE
+            var child = new UnderlyingDictionary
+            {
+            };
+
+            var root = new UnderlyingDictionary
+            {
+                ["child"] = child,
+                ["data"] = 1
+            };
+
+            this.ArrangeFileSystem(root);
+
+            // ACT
+            var result = this.PowerShell.AddCommand("Copy-ItemProperty")
+                .AddParameter("Path", @"test:\")
+                .AddParameter("Name", "data")
+                .AddParameter("Destination", @"test:\child")
+                .Invoke()
+                .ToArray();
+
+            // ASSERT
+            Assert.False(this.PowerShell.HadErrors);
+            Assert.True(root.TryGetValue("data", out var value1));
+            Assert.Equal(1, value1);
+            Assert.True(child.TryGetValue("data", out var value2));
+            Assert.Equal(1, value2);
+        }
+
+        [Fact]
+        public void Powershell_moves_item_property()
+        {
+            // ARRANGE
+            var child = new UnderlyingDictionary
+            {
+            };
+
+            var root = new UnderlyingDictionary
+            {
+                ["child"] = child,
+                ["data"] = 1
+            };
+
+            this.ArrangeFileSystem(root);
+
+            // ACT
+            var result = this.PowerShell.AddCommand("Move-ItemProperty")
+                .AddParameter("Path", @"test:\")
+                .AddParameter("Name", "data")
+                .AddParameter("Destination", @"test:\child")
+                .Invoke()
+                .ToArray();
+
+            // ASSERT
+            Assert.False(this.PowerShell.HadErrors);
+            Assert.False(root.TryGetValue("data", out var _));
+            Assert.True(child.TryGetValue("data", out var value));
+            Assert.Equal(1, value);
         }
     }
 }
